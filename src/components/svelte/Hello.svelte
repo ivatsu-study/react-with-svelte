b6b46959cf07a0ce27b8cec6f4a5a55d24c96907
IvaTsu <ivan.tsud@1337.tech>
2023-02-07 15:12:27 +0100

    feat: add tooltip on Hello component hover

--- src/components/svelte/Hello.svelte
+++ src/components/svelte/Hello.svelte
@@ -2,4 +2,32 @@
   export let text = "";
 </script>
 
-<span class="font-bold">{text}</span>
+<div class="tooltip">
+  <span class="font-bold">{text}</span>
+  <span class="tooltip-text text-sm">I am a Svelte component!</span>
+</div>
+
+<style>
+  .tooltip {
+    position: relative;
+    display: inline-block;
+    border-bottom: 1px dotted black;
+  }
+
+  .tooltip .tooltip-text {
+    visibility: hidden;
+    background-color: black;
+    color: #fff;
+    text-align: center;
+    border-radius: 6px;
+    padding: 5px 0;
+
+    /* Position the tooltip */
+    position: absolute;
+    z-index: 1;
+  }
+
+  .tooltip:hover .tooltip-text {
+    visibility: visible;
+  }
+</style>
